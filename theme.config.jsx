700f9c0b4a4db238290619056be0f49c75b41ae3
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-07-05 19:15:02 +0530

    feat: sidebar collapse btn

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,18 @@
-export default {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-        link: 'https://github.com/shuding/nextra'
-    }
-    // ... other theme options
+const config = {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra'
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
 }
+
+export default config
